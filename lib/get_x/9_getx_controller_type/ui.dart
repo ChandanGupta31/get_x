f00a70c876f824b49a32c4efdcd453d8b4991bfa
ChandanGupta31 <chandankumarguptabgt@gmail.com>
2024-02-05 10:42:42 +0530

    Reactive State GetX Controller Type

---
+++ lib/get_x/9_getx_controller_type/ui.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_x/get_x/9_getx_controller_type/business_logic.dart';
+
+class GetxControllerType extends StatelessWidget {
+  GetxControllerType({super.key});
+
+  // if init property is not used int he GetX<Type of Controller> then
+  // create the instance of controller as follow
+  // MyController myController = Get.put(MyController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GetX(
+                init: MyController(),
+                builder: (controller) {
+                  return Text(
+                    'The Value is ${controller.count}',
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),
+                  );
+                },
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              child: Text('Increase'),
+              onPressed: (){
+                // if the controller is initialize at the top then use this method
+                // myController.increment();
+
+
+                // if the controller is initialize in the body for a widget then use this method
+                Get.find<MyController>().increment();
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
